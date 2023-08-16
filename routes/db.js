f001f7cec80115ad9a7d900360a6f3cc8e467338
aashimap <aashima@bititude.com>
2023-08-16 16:43:45 +0530

    db.js update with knex,bookshelf

--- routes/db.js
+++ routes/db.js
@@ -1,18 +1,33 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Password@1234",
-  database: "training",
+const knex = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    user: "root",
+    password: "Password@1234",
+    database: "training",
+    charset: "utf8",
+  },
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to database:", err);
-    return;
-  }
-  console.log("Connected to database");
-});
+const bookshelf = require("bookshelf")(knex);
+
+module.exports = bookshelf;
+
+// const mysql = require("mysql");
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Password@1234",
+//   database: "training",
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to database:", err);
+//     return;
+//   }
+//   console.log("Connected to database");
+// });
 
 // connection.end((err) => {
 //   if (err) {
@@ -22,4 +37,4 @@ connection.connect((err) => {
 //   console.log("Connection closed");
 // });
 
-module.exports = connection;
+// module.exports = connection;
