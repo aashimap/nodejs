5c87ab2731352fd2623a6c42254e7fd3f846c7db
aashimap <aashima@bititude.com>
2023-08-21 10:03:10 +0530

    create

--- routes/models.js
+++ routes/models.js
@@ -1,35 +1,6 @@
 const bookshelf = require("./db");
 bookshelf.knex.client.config.debug = true;
 
-const Employees = bookshelf.model(
-  "Employees",
-  {
-    tableName: "employees",
-    idAttribute: "id",
-  },
-  {
-    insertEmployee: insertEmployee,
-  }
-);
-
-async function insertEmployee(employeeData) {
-  console.log("employeeData", employeeData);
-  try {
-    const existingEmployee = await Employees.where({
-      id: employeeData.id,
-    }).fetch({ require: false });
-    if (existingEmployee) {
-      throw new Error("ID already exists");
-    }
-    const employee = Employees.forge(employeeData);
-    return employee.save();
-  } catch (error) {
-    throw error;
-  }
-}
-
-module.exports = Employees;
-
-// module.exports = bookshelf.model("Employees", {
-//   tableName: "employees",
-// });
+module.exports = bookshelf.model("Employee", {
+  tableName: "employees",
+});
